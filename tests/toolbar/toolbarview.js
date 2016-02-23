0e095fcacfc35c14c5a358a0ca3f7916c5b8a263
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-02-23 15:53:13 +0100

    Added tests and ckeditor5-core dep.

---
+++ tests/toolbar/toolbarview.js
@@ -0,0 +1,16 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+/* bender-tags: ui, toolbar */
+
+'use strict';
+
+import ToolbarView from '/ckeditor5/ui/toolbar/toolbarview.js';
+
+describe( 'ToolbarView', () => {
+	it( 'exists', () => {
+		expect( ToolbarView ).to.be.a( 'function' );
+	} );
+} );
