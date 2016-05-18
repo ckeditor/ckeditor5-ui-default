d85ac07073a605dd6f9288c909dce4a2ef4445d4
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2016-05-18 16:45:26 +0200

    Added DropdownListItemView and DropdownPanel classes.

---
+++ src/dropdown/dropdownpanel.js
@@ -0,0 +1,27 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+'use strict';
+
+import Controller from '../controller.js';
+import ControllerCollection from '/ckeditor5/ui/controllercollection.js';
+
+/**
+ * The basic dropdown panel controller class.
+ *
+ * @memberOf ui.dropdown
+ * @extends ui.Controller
+ */
+
+export default class DropdownPanel extends Controller {
+	constructor( model, view ) {
+		super( model, view );
+
+		this.collections.add( new ControllerCollection( 'panel' ) );
+
+		// debugger;
+		this.add( 'panel', model.content );
+	}
+}
