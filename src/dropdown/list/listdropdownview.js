bc1f3a555582d63d88adf27878138e3a76ff0d4e
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2016-05-20 11:30:45 +0200

    Added ListDropdownView class with custom DOM listener to close the ListDropdown if user clicked outside of it.

---
+++ src/dropdown/list/listdropdownview.js
@@ -0,0 +1,34 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md.
+ */
+
+'use strict';
+
+import DropdownView from '/ckeditor5/ui/dropdown/dropdownview.js';
+
+/**
+ * The basic list dropdown view class.
+ *
+ * @memberOf ui.dropdown
+ * @extends ui.View
+ */
+
+export default class ListDropdownView extends DropdownView {
+	constructor( model ) {
+		super( model );
+
+		this.listenTo( this.model, 'change:isOn', ( evt, name, value ) => {
+			if ( value ) {
+				// TODO: It will probably be focus/blur-based rather than click.
+				this.listenTo( document, 'click', ( evtInfo, domEvt ) => {
+					if ( this.element != domEvt.target && !this.element.contains( domEvt.target ) ) {
+						this.model.isOn = false;
+					}
+				} );
+			} else {
+				this.stopListening( document, 'click' );
+			}
+		} );
+	}
+}
