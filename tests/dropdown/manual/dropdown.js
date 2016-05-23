9d4cac5db87b0a5c8c0c02882090f0c4e79ee83a
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2016-05-23 15:28:22 +0200

    Tests: Extended Dropdown component manual test.

--- tests/dropdown/manual/dropdown.js
+++ tests/dropdown/manual/dropdown.js
@@ -5,56 +5,70 @@
 
 'use strict';
 
+import testUtils from '/tests/ckeditor5/_utils/utils.js';
+
 import Model from '/ckeditor5/ui/model.js';
 import Collection from '/ckeditor5/utils/collection.js';
-
-import DropdownList from '/ckeditor5/ui/dropdown/dropdownlist.js';
-import DropdownListView from '/ckeditor5/ui/dropdown/dropdownlistview.js';
 
 import Dropdown from '/ckeditor5/ui/dropdown/dropdown.js';
 import DropdownView from '/ckeditor5/ui/dropdown/dropdownview.js';
 
-//---------------------------
+import ListDropdown from '/ckeditor5/ui/dropdown/list/listdropdown.js';
+import ListDropdownView from '/ckeditor5/ui/dropdown/list/listdropdownview.js';
 
-window.Model = Model;
-
-//---------------------------
-
-const itemsCollection = window.itemsCollection = new Collection();
-
-itemsCollection.add( new Model( {
-	label: 'Arial',
-	style: 'font-family: arial'
-} ) );
-
-itemsCollection.add( new Model( {
-	label: 'Times New Roman',
-	style: 'font-family: "Times New Roman"'
-} ) );
-
-itemsCollection.add( new Model( {
-	label: 'Comic Sans',
-	style: 'font-family: "Comic Sans MS"'
-} ) );
-
-const dropdownListModel = new Model( {
-	items: itemsCollection
+testUtils.createTestUIController( {
+	dropdown: 'div#dropdown',
+	listDropdown: 'div#list-dropdown'
+} ).then( ui => {
+	createEmptyDropdown( ui );
+	createListDropdown( ui );
 } );
 
-const dropdownList = new DropdownList( dropdownListModel, new DropdownListView( dropdownListModel ) );
+function createEmptyDropdown( ui ) {
+	const model = new Model( {
+		label: 'Dropdown',
+		isEnabled: true,
+		isOn: false,
+	} );
 
-//---------------------------
+	const dropdown = new Dropdown( model, new DropdownView( model ) );
 
-const dropdownModel = window.dropdownModel = new Model( {
-	label: 'Font',
-	isEnabled: true,
-	isOn: false,
-	content: dropdownList
-} );
+	ui.add( 'dropdown', dropdown );
 
-const dropdown = window.dropdown = new Dropdown( dropdownModel, new DropdownView( dropdownModel ) );
+	dropdown.panel.view.element.innerHTML = 'Empty panel. There is no child view in this DropdownPanelView.';
+}
 
-dropdown.init();
+function createListDropdown( ui ) {
+	const collection = new Collection( { idProperty: 'label' } );
 
-document.body.appendChild( dropdown.view.element );
+	[ '0.8em', '1em', '1.2em', '1.5em', '2.0em', '3.0em' ].forEach( font => {
+		collection.add( new Model( {
+			label: font,
+			style: `font-size: ${ font }`
+		} ) );
+	} );
 
+	const itemListModel = new Model( {
+		items: collection
+	} );
+
+	itemListModel.on( 'execute', ( evtInfo, itemModel ) => {
+		/* global console */
+		console.log( 'ListItem#execute', itemModel );
+	} );
+
+	const model = new Model( {
+		label: 'ListDropdown',
+		isEnabled: true,
+		isOn: false,
+		content: itemListModel
+	} );
+
+	const dropdown = new ListDropdown( model, new ListDropdownView( model ) );
+
+	ui.add( 'listDropdown', dropdown );
+
+	window.listDropdownModel = model;
+	window.listDropdownCollection = collection;
+	window.Model = Model;
+}
