37cd011f9e0a67c0b051c3aab11389cd6483c10c
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2016-05-24 15:25:44 +0200

    Tests: Added List component tests.

--- src/list/list.js
+++ src/list/list.js
@@ -13,11 +13,17 @@ import ListItemView from './listitemview.js';
 /**
  * The basic list controller class.
  *
- * @memberOf ui.dropdown
+ * @memberOf ui.list
  * @extends ui.Controller
  */
 
 export default class List extends Controller {
+	/**
+	 * Creates a List instance.
+	 *
+	 * @param {utils.Observable} model
+	 * @param {ui.View} view
+	 */
 	constructor( model, view ) {
 		super( model, view );
 
@@ -25,31 +31,59 @@ export default class List extends Controller {
 	}
 
 	init() {
-		// Initially populate with children from model.items.
+		// Initially populate "list" controller collection with children from model.items.
 		for ( let itemModel of this.model.items ) {
 			this._addListItem( itemModel );
 		}
 
-		// Synchronize adding to model.items collection with child controllers.
+		// Synchronize adding to model#items collection with "list" controller collection.
 		this.model.items.on( 'add', ( evt, itemModel, index ) => {
 			this._addListItem( itemModel, index );
 		} );
 
-		// Synchronize removal from model.items collection with child controllers.
+		// Synchronize removal from model#items collection with "list" controller collection.
 		this.model.items.on( 'remove', ( evt, itemModel ) => {
-			this.remove( 'list', itemModel );
+			this._removeListItem( itemModel );
 		} );
 
 		return super.init();
 	}
 
+	/**
+	 * Adds an item to "list" collection and activates event bubbling
+	 * between item view and the List.
+	 *
+	 * @protected
+	 * @param {utils.Observable} itemModel
+	 * @param {Number} index
+	 */
 	_addListItem( itemModel, index ) {
-		const view = new ListItemView( itemModel );
+		const itemView = new ListItemView( itemModel );
+		const listItemController = new Controller( itemModel, itemView );
 
-		this.listenTo( view, 'click', () => {
+		// Save model#label in controller instance so it can be later
+		// retrieved from "list" collection easily by that model.
+		listItemController.id = itemModel.label;
+
+		this.listenTo( itemView, 'click', () => {
 			this.model.fire( 'execute', itemModel );
 		} );
 
-		this.add( 'list', new Controller( itemModel, view ), index );
+		this.add( 'list', listItemController, index );
+	}
+
+	/**
+	 * Removes an item from "list" collection and deactivates event bubbling
+	 * between item view and the List.
+	 *
+	 * @protected
+	 * @param {utils.Observable} itemModel
+	 */
+	_removeListItem( itemModel ) {
+		const itemView = this.collections.get( 'list' ).get( itemModel.label ).view;
+
+		this.stopListening( itemView, 'click' );
+
+		this.remove( 'list', itemModel.label );
 	}
 }
