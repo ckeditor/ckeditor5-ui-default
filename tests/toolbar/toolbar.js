6abbfd62337d3d778be956e876d54aa110f00639
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2016-06-24 16:19:49 +0200

    Tests: Updated tests of the Toolbar component.

--- tests/toolbar/toolbar.js
+++ tests/toolbar/toolbar.js
@@ -7,31 +7,20 @@
 
 'use strict';
 
-import Model from '/ckeditor5/ui/model.js';
 import Toolbar from '/ckeditor5/ui/toolbar/toolbar.js';
 import ToolbarView from '/ckeditor5/ui/toolbar/toolbarview.js';
 import ControllerCollection from '/ckeditor5/ui/controllercollection.js';
 
 describe( 'Toolbar', () => {
-	let toolbar, view, model;
+	let toolbar;
 
 	beforeEach( () => {
-		model = new Model( {
-			isActive: false
-		} );
-
-		view = new ToolbarView();
-
-		toolbar = new Toolbar( model, view );
+		toolbar = new Toolbar( new ToolbarView() );
 	} );
 
 	describe( 'constructor', () => {
 		it( 'creates buttons collection', () => {
 			expect( toolbar.collections.get( 'buttons' ) ).to.be.instanceof( ControllerCollection );
-		} );
-
-		it( 'binds view#model attributes to the Toolbar#model', () => {
-			expect( view.model.isActive ).to.equal( model.isActive );
 		} );
 	} );
 } );
