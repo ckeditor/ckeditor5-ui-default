d4f99085653f4845f41b568feac4119fc0e31687
Oskar Wrobel <kontakt@oskarwrobel.pl>
2016-11-03 11:16:19 +0100

    Adjusted LabeledInputView class to be controller less.

--- tests/labeledinput/labeledinputview.js
+++ tests/labeledinput/labeledinputview.js
@@ -5,30 +5,36 @@
 
 /* bender-tags: ui, input */
 
-import LabeledInputView from 'ckeditor5/ui/labeledinput/labeledinputview.js';
-import InputTextView from 'ckeditor5/ui/inputtext/inputtextview.js';
-import LabelView from 'ckeditor5/ui/label/labelview.js';
+import LabeledInputView from '/ckeditor5/ui/labeledinput/labeledinputview.js';
+import InputView from '/ckeditor5/ui/inputtext/inputtextview.js';
+import LabelView from '/ckeditor5/ui/label/labelview.js';
 
 describe( 'InputTextView', () => {
+	const locale = {};
+
 	let view;
 
 	beforeEach( () => {
-		view = new LabeledInputView( InputTextView );
+		view = new LabeledInputView( locale, InputView );
 
 		view.init();
 	} );
 
-	describe( 'constructor()', () => {
-		it( 'should create element from template', () => {
-			expect( view.element.tagName ).to.equal( 'DIV' );
+	describe( 'constructor', () => {
+		it( 'should set view#locale', () => {
+			expect( view.locale ).to.deep.equal( locale );
 		} );
 
-		it( 'should create #labelView', () => {
-			expect( view.labelView ).to.be.instanceOf( LabelView );
+		it( 'should create view#inputView', () => {
+			expect( view.inputView ).to.instanceOf( InputView );
 		} );
 
-		it( 'should create #inputView', () => {
-			expect( view.inputView ).to.be.instanceOf( InputTextView );
+		it( 'should create view#labelView', () => {
+			expect( view.labelView ).to.instanceOf( LabelView );
+		} );
+
+		it( 'should pair inputView and labelView by unique id', () => {
+			expect( view.labelView.for ).to.equal( view.inputView.id ).to.ok;
 		} );
 	} );
 
@@ -39,6 +45,26 @@ describe( 'InputTextView', () => {
 
 		it( 'has input view', () => {
 			expect( view.template.children.get( 1 ) ).to.equal( view.inputView );
+		} );
+	} );
+
+	describe( 'binding', () => {
+		it( 'should bind view#text to view.labelView#text', () => {
+			view.text = 'Foo bar';
+
+			expect( view.labelView.text ).to.equal( 'Foo bar' );
+		} );
+
+		it( 'should bind view#value to view.inputView#value', () => {
+			view.value = 'Lorem ipsum';
+
+			expect( view.inputView.value ).to.equal( 'Lorem ipsum' );
+		} );
+
+		it( 'should bind view.inputView#value to view#value', () => {
+			view.inputView.value = 'Lorem ipsum';
+
+			expect( view.value ).to.equal( 'Lorem ipsum' );
 		} );
 	} );
 
