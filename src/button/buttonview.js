9ec5c2bdb6331b81dcefa0dc58e0124d8d5c6a71
Piotrek Koszuliński <pkoszulinski@gmail.com>
2016-12-01 14:20:07 +0100

    Added comment in the code.

--- src/button/buttonview.js
+++ src/button/buttonview.js
@@ -148,6 +148,8 @@ export default class ButtonView extends View {
 					if ( this.isEnabled ) {
 						this.fire( 'execute' );
 					} else {
+						// Prevent the default when button is disabled, to block e.g.
+						// automatic form submitting. See ckeditor/ckeditor5-link#74.
 						evt.preventDefault();
 					}
 				} )
