8086df9b452fe7f6df62c4c2b9897a0084a632fc
Aleksander Nowodzinski <a.nowodzinski@cksource.com>
2016-12-21 14:14:21 +0100

    Used dataset property instead of attributes in ButtonView tooltip tests.

--- tests/button/buttonview.js
+++ tests/button/buttonview.js
@@ -69,7 +69,7 @@ describe( 'ButtonView', () => {
 
 		describe( 'tooltip', () => {
 			it( 'is not initially set ', () => {
-				expect( view.element.attributes[ 'data-ck-tooltip' ] ).to.undefined;
+				expect( view.element.dataset.ckTooltip ).to.undefined;
 			} );
 
 			it( 'is always equal to view#title if is defined', () => {
@@ -77,20 +77,20 @@ describe( 'ButtonView', () => {
 				view.label = 'foo';
 				view.keystroke = 'A';
 
-				expect( view.element.attributes[ 'data-ck-tooltip' ].value ).to.equal( 'bar' );
+				expect( view.element.dataset.ckTooltip ).to.equal( 'bar' );
 			} );
 
 			it( 'is equal to view#label when view#title is not defined', () => {
 				view.label = 'bar';
 
-				expect( view.element.attributes[ 'data-ck-tooltip' ].value ).to.equal( 'bar' );
+				expect( view.element.dataset.ckTooltip ).to.equal( 'bar' );
 			} );
 
 			it( 'contains keystroke when view#label and view#keystroke is defined', () => {
 				view.label = 'bar';
 				view.keystroke = 'A';
 
-				expect( view.element.attributes[ 'data-ck-tooltip' ].value ).to.equal( 'bar (A)' );
+				expect( view.element.dataset.ckTooltip ).to.equal( 'bar (A)' );
 			} );
 		} );
 
